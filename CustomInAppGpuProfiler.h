5d8484e4296cbcbf2e31cd132d1a287ee8a27c1c
aiekick <aiekick@funparadigm.com>
2024-07-09 22:43:31 +0200

    [FIX] : fix compilation regarding IAGp evolutions

--- CustomInAppGpuProfiler.h
+++ CustomInAppGpuProfiler.h
@@ -24,39 +24,42 @@
 #include <GLFW/glfw3.h>
 
 // #define GPU_CONTEXT void*
-#define GPU_CONTEXT GLFWwindow*
+#define IAGP_GPU_CONTEXT GLFWwindow*
 
 // #define GET_CURRENT_CONTEXT
-static GPU_CONTEXT GetCurrentContext() {
+static IAGP_GPU_CONTEXT GetCurrentContext() {
     return glfwGetCurrentContext();
 }
-#define GET_CURRENT_CONTEXT GetCurrentContext
+#define IAGP_GET_CURRENT_CONTEXT GetCurrentContext
 
 // #define SET_CURRENT_CONTEXT SetCurrentContext
-static void SetCurrentContext(GPU_CONTEXT vContext) {
+static void SetCurrentContext(IAGP_GPU_CONTEXT vContext) {
     glfwMakeContextCurrent(vContext);
 }
-#define SET_CURRENT_CONTEXT SetCurrentContext
+#define IAGP_SET_CURRENT_CONTEXT SetCurrentContext
 
 ////////////////////////////////////////////////////////////////
 // OPTIONNAL ///////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
+// the title of the profiler detail imgui windows
+// #define IAGP_DETAILS_TITLE "Profiler Details"
+
 // the max level of recursion for profiler queries
-// #define RECURSIVE_LEVELS_COUNT 20U
+// #define IAGP_RECURSIVE_LEVELS_COUNT 20U
 
 // the mean average level
 // all the values will be smoothed on 60 frames (1s of 60fps diosplay)
-// #define MEAN_AVERAGE_LEVELS_COUNT 60U
+// #define IAGP_MEAN_AVERAGE_LEVELS_COUNT 60U
 
 // the minimal size of imgui sub window, when you openif by click right on a progiler bar
-#define SUB_AIGP_WINDOW_MIN_SIZE ImVec2(500, 120)
+#define IAGP_SUB_WINDOW_MIN_SIZE ImVec2(500, 120)
 
 // the imgui button to use in IAGP
 // #define IAGP_IMGUI_BUTTON ImGui::Button
 
 // the Imgui Play/Pause button to use
-// #define IMGUI_PLAY_PAUSE_BUTTON
+// #define IAGP_IMGUI_PLAY_PAUSE_BUTTON
 static bool PlayPauseButton(bool& vIsPaused) {
     bool res = false;
     const char* play_pause_label = "Pause";
@@ -72,14 +75,18 @@ static bool PlayPauseButton(bool& vIsPaused) {
     }
     return res;
 }
-#define IMGUI_PLAY_PAUSE_BUTTON PlayPauseButton
+#define IAGP_IMGUI_PLAY_PAUSE_BUTTON PlayPauseButton
 
+// #define IAGP_IMGUI_PLAY_LABEL "Play"
+// #define IAGP_IMGUI_PAUSE_LABEL "Pause"
+// #define IAGP_IMGUI_PLAY_PAUSE_HELP "Play/Pause Profiling"
+// 
 // define your fucntion for log error message of IAGP
 // #define LOG_ERROR_MESSAGE LogError
 #include <cstdio>
-#define LOG_ERROR_MESSAGE printf
+#define IAGP_LOG_ERROR_MESSAGE printf
 
 // define your fucntion for log error message of IAGP only in debug
 // #define LOG_DEBUG_ERROR_MESSAGE LogDebugError
-#define LOG_DEBUG_ERROR_MESSAGE printf
+#define IAGP_LOG_DEBUG_ERROR_MESSAGE printf
 
